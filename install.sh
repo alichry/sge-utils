465da8204a3304b4b928aa9100f8e810f7083a21
Ali Cherry <32630359+alichry@users.noreply.github.com>
2020-04-18 00:06:46 +0300

    fixed installer

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+set -e
+DIR="$(cd -P "$(dirname "$0")" && pwd)"
+
+if [ "$1" = "-h" -o "$1" = "--help"  ]; then
+    echo "Usage: `basename "$0"` <install_path>
+Default install path is /usr/local/bin"
+    exit 0
+fi
 
 install="${1:-/usr/local/bin}"
 
@@ -7,11 +15,13 @@ if [ ! -d "${install}" ]; then
     exit 1
 fi
 
-mkdir -p /etc/sge-utils/templates
-cp conf/jobsub.conf /etc/sge-utils
-cp templates/* /etc/sge-utils/templates
+echo "Installing to ${install}"
 
-for script in src/*.sh
+mkdir -p /etc/sge-utils/templates
+cp "${DIR}/conf/jobsub.conf" /etc/sge-utils
+cp "${DIR}/templates/"* /etc/sge-utils/templates
+
+for script in "${DIR}/src/"*.sh
 do
     dest="${install}/`basename "${script}" .sh`"
     cp "${script}" "${dest}"
