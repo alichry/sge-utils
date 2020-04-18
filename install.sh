3cd5e160e92d5c4879051e4674b1910c65fcc4aa
Ali Cherry <32630359+alichry@users.noreply.github.com>
2020-04-18 16:44:18 +0300

    modified install script

--- install.sh
+++ install.sh
@@ -1,29 +1,57 @@
 #!/bin/sh
-set -e
+set -ex
 DIR="$(cd -P "$(dirname "$0")" && pwd)"
 
-if [ "$1" = "-h" -o "$1" = "--help"  ]; then
-    echo "Usage: `basename "$0"` <install_path>
-Default install path is /usr/local/bin"
-    exit 0
-fi
+install_dir="/usr/local/bin"
+conf_dir="/etc/sge-utils"
 
-install="${1:-/usr/local/bin}"
+printusage () {
+    echo "Usage: `basename "$0"` [-h] [-i INSTALL_DIR] [-c CONF_DIR]
+Defaults are: -i ${install_dir} -c ${conf_dir}"
+}
 
-if [ ! -d "${install}" ]; then
-    echo "error: passed install location is not a directory"
-    exit 1
-fi
+maxshift () {
+    # $1 desired shift
+    # $2 $2 of the caller
+    if [ "$1" -le "$2" ]; then
+        echo "$1"
+    else
+        echo "$2"
+    fi
+}
 
-echo "Installing to ${install}"
+while [ "$#" -gt 0 ];
+do
+    case "$1" in
+        -h|--help)
+            printusage
+            shift 1
+            exit 0
+            ;;
+        -i)
+            install_dir="$2"
+            shift `maxshift 2 "$#"`
+            ;;
+        -c)
+            conf_dir="$2"
+            shift `maxshift 2 "$#"`
+            ;;
+        *)
+            echo "error: invalid argument '$1'" 1>&2
+            exit 1
+            ;;
+    esac
+done
 
-mkdir -p /etc/sge-utils/templates
-cp "${DIR}/conf/jobsub.conf" /etc/sge-utils
-cp "${DIR}/templates/"* /etc/sge-utils/templates
+echo "Installing to ${install_dir} and ${conf_dir}"
+mkdir -p "${install_dir}"
+mkdir -p "${conf_dir}/templates"
+cp "${DIR}/conf/jobsub.conf" "${conf_dir}"
+cp "${DIR}/templates/"* "${conf_dir}/templates"
 
 for script in "${DIR}/src/"*.sh
 do
-    dest="${install}/`basename "${script}" .sh`"
+    dest="${install_dir}/`basename "${script}" .sh`"
     cp "${script}" "${dest}"
     chmod 755 "${dest}"
 done
